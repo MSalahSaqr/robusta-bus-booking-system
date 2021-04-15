b268e3eff9ddbc74db024c6757d6a38fc834ef17
Mohamed Salah <M.salah.saqr@gmail.com>
2021-04-15 07:11:43 +0200

    added HasFactory

--- app/Models/Bus.php
+++ app/Models/Bus.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Bus extends Model
 {
+    use HasFactory;
+
     public function seats()
     {
         return $this->hasMany(Seat::class);
