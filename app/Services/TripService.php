4bf1e4d625a420c7d29c8bd98d8e7fdb0a2458f7
Mohamed Salah <M.salah.saqr@gmail.com>
2021-04-17 23:55:27 +0200

    comments added

--- app/Services/TripService.php
+++ app/Services/TripService.php
@@ -8,6 +8,8 @@ use App\Repositories\Interfaces\ReservationRepositoryInterface;
 use App\Repositories\Interfaces\TripRepositoryInterface;
 use App\Services\Interfaces\TripServiceInterface;
 
+//This class is responsible for business logic related to Trips
+
 class TripService implements TripServiceInterface
 {
 
@@ -17,6 +19,7 @@ class TripService implements TripServiceInterface
         $this->reservationRepo = $reservationRepo;
     }
 
+    //this function returns the trip based on its id with all of its reservations
     private function getTripWithReservations($tripId)
     {
 
@@ -31,9 +34,9 @@ class TripService implements TripServiceInterface
             throw new NotFoundException('Trip not found by ID ' . $tripId);
         }
         return $trip;
-
     }
 
+    //this function returns all seats of a trip based on its id and all of each seat reservations
     private function getSeatswithReservations($tripId)
     {
         $trip = $this->getTripWithReservations($tripId);
@@ -44,6 +47,7 @@ class TripService implements TripServiceInterface
         return $seats;
     }
 
+    //this function extracts the reservations of a specific seat on a trip
     private function getSeatReservations($seatId, $seatsWithReservations)
     {
         $seatInArray = array_filter($seatsWithReservations, function ($item) use ($seatId) {
@@ -56,6 +60,7 @@ class TripService implements TripServiceInterface
         return $seatInArray[0];
     }
 
+    //this function returns an associative array of [stationId => order,... ]
     private function getTripStationsOrder($tripId)
     {
         $stationsOrder = [];
@@ -66,6 +71,7 @@ class TripService implements TripServiceInterface
         return $stationsOrder;
     }
 
+    //this function checks wheather a seat is available or not
     private function CheckSeatAvailablityHelper($seatWithReservations, $fromStationId, $toStationId, $stationsOrders)
     {
         if (!array_key_exists($fromStationId, $stationsOrders) && !array_key_exists($toStationId, $stationsOrders)) {
@@ -102,6 +108,7 @@ class TripService implements TripServiceInterface
         return true;
     }
 
+    //this function prepares the needed date for CheckSeatAvailablityHelper function and calls it
     private function CheckSeatAvailability($seatId, $fromStationId, $toStationId, $tripId)
     {
         $seatsWithReservations = $this->getSeatswithReservations($tripId);
@@ -110,6 +117,7 @@ class TripService implements TripServiceInterface
         return $this->CheckSeatAvailablityHelper($seatWithReservations, $fromStationId, $toStationId, $stationsOrders);
     }
 
+    //this function returns all seats of a specific trip and wither they are available or not
     public function getAllAvailableSeats($fromStationId, $toStationId, $tripId)
     {
         $seats = [];
@@ -125,6 +133,8 @@ class TripService implements TripServiceInterface
         return $seats;
     }
 
+    //this function check weather a seat is available or not and if available reserves it and return the reservation details
+    // and if not returns null
     public function reserveSeat($seatId, $fromStationId, $toStationId, $tripId, $userId)
     {
         if ($this->CheckSeatAvailability($seatId, $fromStationId, $toStationId, $tripId)) {
